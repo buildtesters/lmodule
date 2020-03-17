59d39634a3ab8e2afa38abe89abfca58a79411b7
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-03-17 14:59:01 -0400

    update docstring for all methods, add section in index.rst and README.rst on What is Lmodule and why you would want to use it
    Signed-off-by: Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>

--- lmod/moduleloadtest.py
+++ lmod/moduleloadtest.py
@@ -2,10 +2,12 @@ import os
 from lmod.module import Module
 from lmod.spider import Spider
 
-
 class ModuleLoadTest:
     """This is the class declaration of ModuleLoadTest. This class will automate module load
-    test for all modules in one or more module tree (Software Stack).
+       test for all modules in one or more module tree (Software Stack) retrieved by Spider class. The output
+       of ``module load`` is a 0 or 1 that can be used to determine PASS/FAIL on a module. Each module test will attempt
+       to test the ``module load`` command using the ``Module().test_modules()``. In order to run tests properly,
+       MODULEPATH must be set in your environment.
     """
 
     def __init__(
@@ -20,35 +22,37 @@ class ModuleLoadTest:
         exclude=[],
         login=False,
     ):
-        """This is the initializer method that automates testing of modules
+        """This is the initializer method for ModuleLoadTest class.
 
-        :param tree: specify one or more module trees to test. The module tree must be root directory where modulefiles
-        are found. Use a colon ``:`` to define more than one module tree.
-        :type tree: str
+           Parameters:
+           -----------
+           :param tree: specify one or more module trees to test. The module tree must be root directory where modulefiles
+            are found. Use a colon ``:`` to define more than one module tree.
+           :type tree: str
 
-        :param purge: control whether to run ``module purge`` before loading each module
-        :type purge: bool
+           :param purge: control whether to run ``module purge`` before loading each module
+           :type purge: bool
 
-        :param force: control whether to run ``module --force purge`` before loading each module
-        :type purge: bool
-        
-        :param login: control whether to run test in login shell. Defaults to sub-shell when ``login=False`` 
-        :type purge: bool
-        
-        :param count: control how many tests to run before exiting
-        :type purge: int
+           :param force: control whether to run ``module --force purge`` before loading each module
+           :type purge: bool
 
-        :param name: filter modules by software name to test
-        :type name: list
+           :param login: controls whether to run test in login shell when ``login=True``. By default tests are run in sub-shell.
+           :type purge: bool
 
-        :param include: specify a list of modules to **include** by full canonical name for testing
-        :type purge: list
+           :param count: control how many tests to run before exiting
+           :type purge: int
 
-        :param exclude: specify a list of modules to **exclude** by full canonical name for testing
-        :type purge: list
+           :param name: filter modules by software name to test
+           :type name: list
 
-        :return: Result of module load test
-        :rtype: None
+           :param include: specify a list of modules to **include** by full canonical name for testing
+           :type purge: list
+
+           :param exclude: specify a list of modules to **exclude** by full canonical name for testing
+           :type purge: list
+
+           :return: Result of module load test
+           :rtype: None
         """
 
         # setting module tree to argument passed in or default to MODULEPATH
@@ -76,8 +80,10 @@ class ModuleLoadTest:
         modules = filter_modules or modules
 
         modulecount = 0
+
         print(f"Testing the Following Module Trees: {self.tree}")
         print("{:_<80}".format(""))
+
         for module_name in modules:
             module_cmd = Module(
                 module_name, purge=self.purge, force=self.force, debug=self.debug, 
