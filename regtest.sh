5d9a6cd438d9890d3863b424044e6c464d274cc8
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-03-17 16:23:29 -0400

    troubleshooting travis
    Signed-off-by: Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>

--- regtest.sh
+++ regtest.sh
@@ -1,12 +1,3 @@
 #!/bin/bash 
-set -ev
-source /opt/apps/lmod/lmod/init/profile
-module use $LMOD_PKG/modulefiles/Core
-echo $MODULEPATH
-cd $TRAVIS_BUILD_DIR
-echo $MODULEPATH
-echo $BASH_ENV
-env | grep LMOD
-module av
 coverage run -m pytest -vra tests/
 coverage report -m
